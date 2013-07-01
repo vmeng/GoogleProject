1ca21ddc4f86fdab2701efb12d1009eef5b18083
vmeng <jmeng2007@163.com>
2013-07-01 17:37:05 +0800

    add ruby code

---
+++ Ruby/speech2text/Gemfile
@@ -0,0 +1,5 @@
+source :rubygems
+
+gem 'uuid'
+gem 'curb'
+gem 'json'
---
+++ Ruby/speech2text/lib/speech.rb
@@ -0,0 +1,11 @@
+# -*- encoding: binary -*-
+
+require 'curb'
+require 'json'
+require 'uuid'
+
+module Speech; end
+
+require_relative './speech/audio_inspector'
+require_relative './speech/audio_splitter'
+require_relative './speech/audio_to_text'
